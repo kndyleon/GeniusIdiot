c658cbd28f61acd09b86aad1ab6ed38d78bf481a
Nick <Nick@LAPTOP-P4AT0N4D>
2024-02-26 20:43:53 +0300

    Убрал повторение вопросов.

--- GeniusIdiotConsoleApp/Program.cs
+++ GeniusIdiotConsoleApp/Program.cs
@@ -1,30 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GeniusIdiotConsoleApp
 {
     internal class Program
     {
-        static string[] GetQuestions(int countQuestions)
+        static List<string> GetQuestions(int countQuestions)
         {
-            string[] questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса, сколько нужно минут для трёх уколов?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-
+            List<string> questions = new List<string>(countQuestions)
+            {
+                "Сколько будет два плюс два умноженное на два?",
+                "Бревно нужно распилить на 10 частей, сколько нужно сделать распилов?",
+                "На двух руках 10 пальцев. Сколько пальцев на 5 руках?",
+                "Укол делают каждые полчаса, сколько нужно минут для трёх уколов?",
+                "Пять свечей горело, две потухли. Сколько свечей осталось?"
+            };
             return questions;
         }
 
-        static int[] GetAnswers(int countQuestions) 
+        static List<int> GetAnswers(int countQuestions) 
         {
-            int[] answers = new int[countQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-
+            List<int> answers = new List<int>(countQuestions)
+            {
+                6,
+                9,
+                25,
+                60,
+                2
+            };
             return answers;
         }
 
@@ -46,14 +49,15 @@ namespace GeniusIdiotConsoleApp
         static void Main(string[] args)
         {
             int countQuestions = 5;
-            string[] questions = GetQuestions(countQuestions);
-            int[] answers = GetAnswers(countQuestions);
+            List<string> questions = GetQuestions(countQuestions);
+            List<int> answers = GetAnswers(countQuestions);
             int countRightAnswers = 0;
             Random random = new Random();
+            int countLeftQuestions = countQuestions;
 
             for (int i = 0; i < countQuestions; i++)
             {
-                int randomQuestionIndex = random.Next(0, countQuestions);
+                int randomQuestionIndex = random.Next(0, countLeftQuestions);
                 Console.WriteLine($"Вопрос №{i + 1}. {questions[randomQuestionIndex]}");
 
                 int userAnswer = Convert.ToInt32(Console.ReadLine());
@@ -63,6 +67,9 @@ namespace GeniusIdiotConsoleApp
                 {
                     countRightAnswers++;
                 }
+                countLeftQuestions--;
+                questions.RemoveAt(randomQuestionIndex);
+                answers.RemoveAt(randomQuestionIndex);
             }
             
             string diagnosis = GetDiagnosis(countRightAnswers);
